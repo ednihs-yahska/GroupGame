8ceb83e1dc9b31e4799054372710fe5739a79c62
Michael Wang <wangshiyao96@gmail.com>
2019-04-30 00:14:19 -0700

    Update to Unity 2019

--- GroupGame/Assets/Scripts/Character/CharacterMovement.cs
+++ GroupGame/Assets/Scripts/Character/CharacterMovement.cs
@@ -6,29 +6,44 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed = 3.0F;          //the moving speed of the character
-
-    public float rotateSpeed = 3.0F;    //the rotate speed of the character
-
-    public float jumpSpeed = 10.0f;      //the jump force of the character
-    private int jumpFlag = 0;
-    private float jumpStartT = 0.4f;
-
-    public float gravity = 20.0f;       //the force of gravity on the character
-
-    public float GroundOffset = .2f;    //the offset for the IsGrounded check. Useful for recognizing slopes and imperfect ground.
+    public float speed = 3.0F;              //the moving speed of the character
+    public float run_factor = 1.6f;         //The speed up factor when running
+    public float slide_factor = 1.6f;       //The speed up factor when slide while running
+    public float rotateSpeed = 3.0F;        //the rotate speed of the character
+    public float jumpSpeed = 5.0f;          //the jump force of the character
+    public float gravity = 9.8f;            //the force of gravity on the character
+    public float GroundOffset = .2f;        //the offset for the IsGrounded check. Useful for recognizing slopes and imperfect ground.
 
     private Vector3 moveDirection = Vector3.zero;   //the direction the character should move.
-
     private Vector3 jumpDirection = Vector3.zero;
+    private CharacterController controller;         //The character controller object
+    private float rotationX, rotationY;             //The rotation parameter
 
-    private CharacterController controller;
+    private Animator anim;                  //The current animator object
+    private float Horizontal, Vertical;     //The input from player
+    private bool Run, Jump, Slide;          //The flags to control the animation
 
-    private GameObject head;      //The main camera of player, used to rotate the camera
 
-    //private GameObject gun;
+    //The check to see if the character is currently on the ground.
+    private bool isGrounded()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, -transform.up, out hit, 10);                //A short ray shot directly downward from the center of the character.
 
-    private float rotationX, rotationY;        //The rotation parameter
+        if (System.Math.Abs(hit.distance) < System.Single.Epsilon)                      //if the distance is zero, the ray probably did not hit anything.
+        {
+            return false;
+        }
+
+        //If the distance from the ray is less than half the height of the character (plus the offset), the character us grounded.
+            if (hit.distance <= (this.transform.lossyScale.y / 2 + GroundOffset))  
+        {
+            Debug.Log("On the ground!");
+            return true;
+        }
+        Debug.Log("Not on the ground!");
+        return false;
+    }
 
 
     //This built-in function will be called after the script first time loaded into the scene
@@ -38,63 +53,87 @@ public class CharacterMovement : MonoBehaviour
 
         controller = GetComponent<CharacterController>();
 
-        head = this.gameObject.transform.GetChild(0).gameObject;
-    }
+        anim = gameObject.GetComponent<Animator>();     //Get the animator object
 
-    ///The check to see if the character is currently on the ground.
-    private bool isGrounded(){
-        RaycastHit hit;
-        Physics.Raycast(this.transform.position, -this.transform.up,out hit, 10);   //A short ray shot directly downward from the center of the character.
-
-        if(System.Math.Abs(hit.distance) < System.Single.Epsilon)                                           //if the distance is zero, the ray probably did not hit anything.
-        {
-            return false;
-        }
-        if(hit.distance <= (this.transform.lossyScale.y/2 +GroundOffset))   //if the distance from the ray is less than half the height 
-        {                                                                   //of the character (plus the offset), the character us grounded.
-            return true;
-        }
-        return false;
+        Run = Jump = Slide = false;                     //Initialize the flag
     }
 
     void Update()
     {
-        // Rotate the object
+        //Get the value for each axis from input
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        //Rotate the character
         rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotateSpeed;
         rotationY -= Input.GetAxis("Mouse Y") * rotateSpeed;
-        transform.localEulerAngles = new Vector3(0, rotationX, 0);
-        head.transform.localEulerAngles = new Vector3(rotationY, 0, 0);
-        //gun.transform.localEulerAngles = new Vector3(rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(rotationY, rotationX, 0);
 
 
-        // Move the character     
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));       //Create the player's movement from keyboard in local space
-        moveDirection = transform.TransformDirection(moveDirection);      //Transform the moveMent from local space to world space
+        /********** 2D Plane Movement **********/
+
+        moveDirection = new Vector3(Horizontal, 0.0f, Vertical);            //Create the player's movement from keyboard in local space
+        moveDirection = transform.TransformDirection(moveDirection);        //Transform the moveMent from local space to world space
         moveDirection *= speed;      //Based on base speed
 
-        if (Input.GetButtonDown ("Jump"))               //jump if the character is grounded and the user presses the jump button.
+        //If player is walking and is not running
+        if (Vertical != 0f)
         {
-            jumpDirection.y = jumpSpeed;     //Give a jump speed to player
-            //jumpFlag = 1;
-        }
-        /*
-        if(jumpFlag == 1)
-        {
-            jumpStartT -= Time.deltaTime;   
-            if(jumpStartT <= 0)
+            if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                jumpDirection.y = jumpSpeed;     //Give a jump speed to player
-                jumpFlag = 0;
-                jumpStartT = 0.4f;
+                Run = !Run;
             }
         }
-        */
 
+        //If the player is running and hit ctrl, then slide
+        if (Run)
+        {
+            moveDirection *= run_factor;        //If the player is running, then speed up
+
+            //If the player choose to slide
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                Slide = true;
+                moveDirection *= slide_factor;  //If the player is sliding, then speed up
+            }
+            else
+            {
+                Slide = false;
+            }
+
+            if (Vertical == 0)
+            {
+                Run = false;
+            }
+        }
+
+
+
+        /********** Verticle Movement **********/
+
+        //If the player hit the space, then enter the jump animation
+        if (Input.GetButton("Jump") && isGrounded())
+        {
+            Jump = true;                        //Set the jump flag
+            jumpDirection.y = jumpSpeed;        //Give a jump speed to player
+        }
+        else
+        {
+            Jump = false;
+        }
+
+        //Add gravity and move the character
         controller.Move(moveDirection * Time.deltaTime);    //move the character based on the gravitational force.
-
         jumpDirection.y -= gravity * Time.deltaTime;
-
         controller.Move(jumpDirection * Time.deltaTime);
-    }
 
+
+        /********** Animation Part **********/
+
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetBool("Jump", Jump);
+        anim.SetBool("Run", Run);
+        anim.SetBool("Slide", Slide);
+    }
 }
---
+++ GroupGame/Assets/Scripts/Character/ThirdController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdController: MonoBehaviour
+{
+
+    private Animator anim;                  //The current animator object
+    private Rigidbody rb;                   //The current rigidbody object
+    private float Horizontal, Vertical;     //The input from player
+    private bool Run, Jump, Slide;          //The flags to control the animation
+    private float speed;                    //The real speed of the player inside the game, the combination of move_speed and run_factor
+
+    public float move_speed;                //The moving speed of player
+    public float run_factor;                //The multipler to the speed when player run
+    public float jump_force;                //The force will applyed to player when jump
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();     //Get the animator object
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        Run = Jump = Slide = false;     //Initialize the flag     
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /********** Horizontal Part **********/
+
+        //Get the value for each axis from input
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        //If player is walking and is not running
+        if(Vertical != 0f)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Run = !Run;
+            }
+        }
+
+        //If the player is running and hit ctrl, then slide
+        if(Run)
+        {
+            speed *= run_factor;        //Increase the speed
+
+            if(Input.GetKey(KeyCode.LeftControl))
+            {
+                Slide = true;
+            }
+            else
+            {
+                Slide = false;
+            }
+
+            if(Vertical == 0)
+            {
+                Run = false;
+            }
+        }
+
+        /********** Verticle Part **********/
+
+        //If the player hit the space, then enter the jump animation
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump = true;
+        }
+        else
+        {
+            Jump = false;
+        }
+
+        /********** Animation Part **********/
+
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetBool("Jump", Jump);
+        anim.SetBool("Run", Run);
+        anim.SetBool("Slide", Slide);
+	}
+}
